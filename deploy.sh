13191ebbc12b40f476d6f483a69351757c1943bf
Matthew Sarver <msarver@its.jnj.com>
2016-10-10 20:11:45 -0400

    Added deploy script and updated gitignore

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+# platform detection
+platform='unknown'
+unamestr=$(uname)
+if [[ "$unamestr" == "Linux" ]]; then
+    platform='linux'
+elif [[ "$unamestr" == "Darwin" ]]; then
+    platform='osx'
+fi
+
+# use macvim on osx
+if [[ $platform == 'osx' ]]; then
+    alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
+fi
+
+for path in .vimrc .vim .bash_aliases .bash_profile .bashrc; do
+    echo $path
+    if [ ! -h ~/$path ]; then
+        ln -vis ~/.dotfiles/$path ~/$path
+    fi
+done
+
+rm -rf ~/.vim/bundle/Vundle.vim && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || true
+curl https://raw.githubusercontent.com/baskerville/bubblegum/master/colors/bubblegum-256-dark.vim > ~/.vim/colors/bubblegum-256-dark.vim
+vim +BundleInstall +qall
+unalias vim
+echo "Deploy done!"
